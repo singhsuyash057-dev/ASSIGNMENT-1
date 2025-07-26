32b281ab4d4d3bc436cf41031a928f85932fcd3e
singhsuyash057-dev <singhsuyash057@gmail.com>
2025-07-26 12:25:07 +0530

    Add files via upload
    ASSIGNMENT1
    Perform Basic Mathematical Operations (TASK1)
    Create a Personalized Greeting(TASK2)

---
+++ assignment1task1.py
@@ -0,0 +1,12 @@
+a=input("enter the first number:")
+a=float(a)
+b=input("enter the second number:")
+b=float(b)
+A=a+b
+B=a-b
+C=a*b
+D=a/b
+print("addition:", A)
+print("subtraction:", B)
+print("multiplication:", C)
+print("division:", D)
---
+++ assignment2task2.py
@@ -0,0 +1,3 @@
+a=input("enter ur first name:")
+b=input("enter ur second name:")
+print("hello,",a, b,"welcome to the python program!")
